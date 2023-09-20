8ac61b3f8392bba76f462ee5c05e0a56890925f5
Zaid <zedpx101@outlook.com>
2023-09-20 11:49:47 +0100

    add boo connection

--- src/server.js
+++ src/server.js
@@ -1,10 +1,26 @@
+require("dotenv").config();
 const express = require("express");
 
-const port = 80;
+const port = process.env. PORT || 5001;
 
 const app = express();
 
 app.use(express.json());
+
+const {DataTypes} = require("sequelize")
+const connection = requuire("./db/connection");
+
+const Book = connection.define("Book", {
+    title: {
+        type: DataTypes.STRING,
+    },
+    author: {
+        type: DataTypes.STRING,
+    },
+    GENRE: {
+        type: DataTypes.STRING,
+    }
+});
 
 app.listen(port, () => {
     console.log(`App is listening on ${port}`)
