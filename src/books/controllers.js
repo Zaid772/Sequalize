3c00932c1afca7acc33a894f2b41399a3ae4c6dc
Zaid <zedpx101@outlook.com>
2023-09-24 17:54:50 +0100

    refactor: everything from routes.js to controllers.js

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -12,7 +12,7 @@ const addBooks = async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: error, errorMessage: error.message })
     }
-}
+};
 
 const getAllBooks = async (req, res) => {
     try {
@@ -24,7 +24,37 @@ const getAllBooks = async (req, res) => {
     }
 };
 
+const updateBook = async (req, res) => {
+    try {
+        const updateaBook = await Book.update(
+            { author: req.body.newAuthor},
+            { which: { title: req.body.title } }
+        );
+
+        res.status(200).json({ updated: updateaBook, message: "book updated" });
+    } catch (error) {
+        res.status(500).json({ error: error, errorMessage: error.message });
+    }
+};
+
+const deleteBook = async (req, res) => {
+    try {
+        const deleteaBook = await Book.destroy({
+            title: req.body.title,
+            author: req.body.author,
+            genre: req.body.genre
+        });
+    
+        res.status(200).json({ deleted: deleteaBook, message: "book deleted"});
+    } catch (error) {
+        res.status(500).json({ error: error, errorMessage: error.message });
+    }
+};
+
+
 module.exports = {
     addBooks: addBooks,
     getAllBooks: getAllBooks,
-}
+    updateBook: updateBook,
+    deleteBook: deleteBook
+};
--- src/books/routes.js
+++ src/books/routes.js
@@ -1,40 +1,12 @@
 const { Router }= require("express");
 const bookRouter = Router();
 
-const Book = require("./model");
-
-const { getAllBooks, addBooks } = require("./controllers")
+const { getAllBooks, addBooks, updateBook, deleteBook } = require("./controllers")
 
 bookRouter.post("/addbook", addBooks);
-
 bookRouter.get("/listallbooks", getAllBooks);
-
-bookRouter.delete("/deleteabook", async (req, res) => {
-    const deleteaBook = await Book.destroy({
-        title: req.body.title,
-        author: req.body.author,
-        genre: req.body.genre
-    })
-    const successResponse = {
-        deleted: deleteaBook,
-        message: "book deleted"
-    };
-
-    res.status(201).json(successResponse);
-});
-
-bookRouter.put("/updateabook", async (req, res) => {
-    const updateaBook = await Book.update(
-        { author: req.body.newAuthor},
-        { which: { title: req.body.title } }
-    );
-    const successResponse = {
-        updated: updateaBook,
-        message: "book updated"
-    };
-
-    res.status(201).json(successResponse);
-});
+bookRouter.put("/updateabook", updateBook);
+bookRouter.delete("/deleteabook", deleteBook);
 
 
 module.exports = bookRouter;
